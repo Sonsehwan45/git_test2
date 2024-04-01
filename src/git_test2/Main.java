ecea0e3417f5875d3974fe2ba41dc6ffee019358
Son Sehwan <sahwan45@naver.com>
2024-04-01 12:48:21 +0900

    Main.java. 재수정

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -2,8 +2,8 @@ package git_test2;
 
 public class Main {
     public static void main(String[] args) {
-    	Hello hello = new Hello();
-        hello.say();
+    	Hello hello1 = new Hello();
+        hello1.say();
 
     }
 
